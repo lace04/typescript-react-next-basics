0bc1147b75c0266efb30533ca95ec59a4c897a0b
lace04 <luisace04@gmail.com>
2025-02-22 19:48:43 -0500

    Actualizar estilos globales y agregar componente de botón en la página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+
+let title: string = 'Hola mundo';
+
+function sumar(a: number, b: number): number {
+  return a + b;
+}
+
+function Button() {
+  console.log(sumar(1, 2));
+  return <button>{title}</button>;
+}
 
 function page() {
   return (
-    <div>Bienvenido a el curso!</div>
-  )
+    <>
+      <div>Bienvenido a el curso!</div>
+      <Button />
+    </>
+  );
 }
 
-export default page
+export default page;
