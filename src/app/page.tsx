fbc49df783b97ba6f235af754ebc75dcf90d2fa5
lace04 <luisace04@gmail.com>
2025-02-22 19:50:34 -0500

    Agregar objeto de usuario y limpiar el componente de botón

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 let title: string = 'Hola mundo';
 
+let user = {
+  name: 'Juan',
+  age: 25,
+}
+
 function sumar(a: number, b: number): number {
   return a + b;
 }
 
 function Button() {
+
+  
+
   console.log(sumar(1, 2));
   return <button>{title}</button>;
 }
